06c2576a3621dbd9324b9deb9e979113746912e3
jimmy4834 <jimmy4834@hotmail.com>
2019-05-25 12:37:54 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-var id1='178523', id2='178524', captcha='20190629';
+var id1='181357', captcha='20190629';
 
 if ($("i.fa-plus").length == 0) {
   location.reload()
 }
 
-$("#person_agree_terms").click()
 $("#ticket_"+id1).find("i.fa-plus").click();
-$("#ticket_"+id2).find("i.fa-plus").click();
 $("input[name=captcha_answer]")[0].scrollIntoView();
 $("input[name=captcha_answer]").val(captcha);
 $("input[name=captcha_answer]").change();
+$("#person_agree_terms").click();
 $('button.btn.btn-primary').click();
