6b9a8bcfbf52a31fd35090e1f8b533e948943f88
jimmy4834 <jimmy4834@hotmail.com>
2019-06-29 10:43:34 +0800

    auto click first plus and modify default captcha

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-var id1='181357', captcha='20190629';
+var captcha='C3c';
 
 if ($("i.fa-plus").length == 0) {
   location.reload()
 }
 
-$("#ticket_"+id1).find("i.fa-plus").click();
+$("i.fa-plus")[0].click()
 $("input[name=captcha_answer]")[0].scrollIntoView();
 $("input[name=captcha_answer]").val(captcha);
 $("input[name=captcha_answer]").change();
