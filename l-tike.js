b43b24e6cb92f38f2bd4e4daf48f87b46838c75c
Han Chiang <jimmy4834@hotmail.com>
2024-08-20 22:43:07 +0800

    Update l-tike.js

--- l-tike.js
+++ l-tike.js
@@ -3,4 +3,35 @@ var mail='';
 var tel='';
 $('#MAIL_ADDRS').val(mail)
 $('#MAIL_ADDRS_CONFIRM').val(mail)
+$('#TEL').val(tel)
+$('#TEL_CONFIRM').val(tel)
+$('#NEXT').click()
+
+// application page
+var pwd='';
+var family='';
+var first='';
+var by='';
+var bm='';
+var bd='';
+$('#PWD').val(pwd)
+$('#PWD_CNF').val(pwd)
+$('#APLCT_FIRST_NAME').val(family)
+$('#APLCT_LAST_NAME').val(first)
+$('#APLCT_BIRTHDAY_YEAR').val(by)
+$('#APLCT_BIRTHDAY_MONTH').val(bm)
+$('#APLCT_BIRTHDAY_DAY').val(bd)
+$('#NEXT_BUTTON').click()
+
+// C 
+var last='';
+var first='';
+var by='';
+var bm='';
+var bd='';
+$('#c_1_name_family').val(last)
+$('#c_1_name_first').val(first)
+$('#c_1_birthDay_year').val(by)
+$('#c_1_birthDay_month').val(bm)
+$('#c_1_birthDay_day').val(bd)
 $('#NEXT').click()
